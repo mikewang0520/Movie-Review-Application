72cbdf74c363276895117d80648589d3a5aaaebf
mikewang0520 <mikewang2000@outlook.com>
2023-12-09 22:29:21 -0500

    switch to static ngrok url

--- MovieClient/movie-gold-v1/src/api/axiosConfig.js
+++ MovieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: 'http://localhost:8080',
-    baseURL: 'https://2a63-173-63-49-50.ngrok-free.app',
+    baseURL: 'https://patient-composed-doberman.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
