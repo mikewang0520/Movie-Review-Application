9c48e7b3508dc85aeb0bec73c0c6c0e6dfedc80f
mikewang0520 <mikewang2000@outlook.com>
2023-12-14 18:05:23 -0500

    Create review functionality

--- MovieClient/movie-gold-v1/src/components/trailer/Trailer.js
+++ MovieClient/movie-gold-v1/src/components/trailer/Trailer.js
@@ -2,20 +2,19 @@ import {useParams} from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import './Trailer.css';
 
-
-import React from 'react';
+import React from 'react'
 
 const Trailer = () => {
-    
+
     let params = useParams();
     let key = params.ytTrailerId;
 
-    return (
-        <div className="react-player-container">
-            {(key!=null)?<ReactPlayer controls="true" playin={true} url={`https://www.youtube.com/watch?v=${key}`}
-            width='100%' height='100%'/>:null}
-        </div>
-    )
+  return (
+    <div className="react-player-container">
+      {(key!=null)?<ReactPlayer controls="true" playing={true} url ={`https://www.youtube.com/watch?v=${key}`} 
+      width = '100%' height='100%' />:null}
+    </div>
+  )
 }
 
 export default Trailer
--- MovieClient/movie-gold-v1/src/index.js
+++ MovieClient/movie-gold-v1/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<App/>}/>      
+          <Route path="/*" element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
